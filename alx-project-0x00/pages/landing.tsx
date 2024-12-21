8c0f8ff842111cda4efd8159599264509795171b
efezino <enduranceidisi13@gmail.com>
2024-12-21 13:48:46 +0100

    complete all tasks

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/Button';
 import Card from '@/components/Card';
 
 const Landing: React.FC = () => {
@@ -5,6 +6,18 @@ const Landing: React.FC = () => {
     <div>
       <h1 className=' text-xl font-extralight'>Landing Page</h1>
       <Card />
+
+      <div className='flex gap-4'>
+        <Button title='small' styles='rounded-sm bg-blue-500 p-1 capitalize' />
+        <Button
+          title='medium'
+          styles='rounded-md bg-purple-500 p-1 capitalize text-xl'
+        />
+        <Button
+          title='large'
+          styles='rounded-lg bg-yellow-500 p-1 capitalize text-2xl'
+        />
+      </div>
     </div>
   );
 };
